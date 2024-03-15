26bf2696fb84d67cb891ca7f098438c665cb93dd
Quynh Tong <98098589+dieuquynh1903@users.noreply.github.com>
2024-03-15 23:13:33 +0700

    social media web

---
+++ README.md
@@ -0,0 +1,31 @@
+## Technologies
+
+- React.js
+- Appwrite
+- React Query
+- TypeScript
+- Shadcn
+- Tailwind CSS
+
+## Features
+
+**Authentication System**: A robust authentication system ensuring security and user privacy
+
+**Explore Page**: Homepage for users to explore posts, with a featured section for top creators
+
+**Like and Save Functionality**: Enable users to like and save posts, with dedicated pages for managing liked and saved content
+
+**Detailed Post Page**: A detailed post page displaying content and related posts for an immersive user experience
+
+**Profile Page**: A user profile page showcasing liked posts and providing options to edit the profile
+
+**Browse Other Users**: Allow users to browse and explore other users' profiles and posts
+
+**Create Post Page**: Implement a user-friendly create post page with effortless file management, storage, and drag-drop feature
+
+**Edit Post Functionality**: Provide users with the ability to edit the content of their posts at any time
+
+**Responsive UI with Bottom Bar**: A responsive UI with a bottom bar, enhancing the mobile app feel for seamless navigation
+**React Query Integration**: Incorporate the React Query (Tanstack Query) data fetching library for, Auto caching to enhance performance, Parallel queries for efficient data retrieval, First-class Mutations, etc
+
+**Backend as a Service (BaaS) - Appwrite**: Utilize Appwrite as a Backend as a Service solution for streamlined backend development, offering features like authentication, database, file storage, and more and many more, including code architecture and reusability
---
+++ src/_root/pages/Profile.tsx
@@ -0,0 +1,303 @@
+import { useParams, Link, useNavigate } from "react-router-dom";
+
+import { Button } from "@/components/ui";
+import { Loader } from "@/components/shared";
+import { GridPostList, PostStats } from "@/components/shared";
+
+import {
+  useGetPostById,
+  useGetUserPosts,
+  useDeletePost,
+} from "@/lib/react-query/queries";
+import { multiFormatDateString } from "@/lib/utils";
+import { useUserContext } from "@/context/AuthContext";
+
+const PostDetails = () => {
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const { user } = useUserContext();
+
+  const { data: post, isLoading } = useGetPostById(id);
+  const { data: userPosts, isLoading: isUserPostLoading } = useGetUserPosts(
+    post?.creator.$id
+  );
+  const { mutate: deletePost } = useDeletePost();
+
+  const relatedPosts = userPosts?.documents.filter(
+    (userPost) => userPost.$id !== id
+  );
+
+  const handleDeletePost = () => {
+    deletePost({ postId: id, imageId: post?.imageId });
+    navigate(-1);
+  };
+
+  return (
+    <div className="post_details-container">
+      <div className="hidden md:flex max-w-5xl w-full">
+        <Button
+          onClick={() => navigate(-1)}
+          variant="ghost"
+          className="shad-button_ghost">
+          <img
+            src={"/assets/icons/back.svg"}
+            alt="back"
+            width={24}
+            height={24}
+          />
+          <p className="small-medium lg:base-medium">Back</p>
+        </Button>
+      </div>
+
+      {isLoading || !post ? (
+        <Loader />
+      ) : (
+        <div className="post_details-card">
+          <img
+            src={post?.imageUrl}
+            alt="creator"
+            className="post_details-img"
+          />
+
+          <div className="post_details-info">
+            <div className="flex-between w-full">
+              <Link
+                to={`/profile/${post?.creator.$id}`}
+                className="flex items-center gap-3">
+                <img
+                  src={
+                    post?.creator.imageUrl ||
+                    "/assets/icons/profile-placeholder.svg"
+                  }
+                  alt="creator"
+                  className="w-8 h-8 lg:w-12 lg:h-12 rounded-full"
+                />
+                <div className="flex gap-1 flex-col">
+                  <p className="base-medium lg:body-bold text-light-1">
+                    {post?.creator.name}
+                  </p>
+                  <div className="flex-center gap-2 text-light-3">
+                    <p className="subtle-semibold lg:small-regular ">
+                      {multiFormatDateString(post?.$createdAt)}
+                    </p>
+                    •
+                    <p className="subtle-semibold lg:small-regular">
+                      {post?.location}
+                    </p>
+                  </div>
+                </div>
+              </Link>
+
+              <div className="flex-center gap-4">
+                <Link
+                  to={`/update-post/${post?.$id}`}
+                  className={`${user.id !== post?.creator.$id && "hidden"}`}>
+                  <img
+                    src={"/assets/icons/edit.svg"}
+                    alt="edit"
+                    width={24}
+                    height={24}
+                  />
+                </Link>
+
+                <Button
+                  onClick={handleDeletePost}
+                  variant="ghost"
+                  className={`ost_details-delete_btn ${
+                    user.id !== post?.creator.$id && "hidden"
+                  }`}>
+                  <img
+                    src={"/assets/icons/delete.svg"}
+                    alt="delete"
+                    width={24}
+                    height={24}
+                  />
+                </Button>
+              </div>
+            </div>
+
+            <hr className="border w-full border-dark-4/80" />
+
+            <div className="flex flex-col flex-1 w-full small-medium lg:base-regular">
+              <p>{post?.caption}</p>
+              <ul className="flex gap-1 mt-2">
+                {post?.tags.map((tag: string, index: string) => (
+                  <li
+                    key={`${tag}${index}`}
+                    className="text-light-3 small-regular">
+                    #{tag}
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <div className="w-full">
+              <PostStats post={post} userId={user.id} />
+            </div>
+          </div>
+        </div>
+      )}
+
+      <div className="w-full max-w-5xl">
+        <hr className="border w-full border-dark-4/80" />
+
+        <h3 className="body-bold md:h3-bold w-full my-10">import {
+  Route,
+  Routes,
+  Link,
+  Outlet,
+  useParams,
+  useLocation,
+} from "react-router-dom";
+
+import { Button } from "@/components/ui";
+import { LikedPosts } from "@/_root/pages";
+import { useUserContext } from "@/context/AuthContext";
+import { useGetUserById } from "@/lib/react-query/queries";
+import { GridPostList, Loader } from "@/components/shared";
+
+interface StabBlockProps {
+  value: string | number;
+  label: string;
+}
+
+const StatBlock = ({ value, label }: StabBlockProps) => (
+  <div className="flex-center gap-2">
+    <p className="small-semibold lg:body-bold text-primary-500">{value}</p>
+    <p className="small-medium lg:base-medium text-light-2">{label}</p>
+  </div>
+);
+
+const Profile = () => {
+  const { id } = useParams();
+  const { user } = useUserContext();
+  const { pathname } = useLocation();
+
+  const { data: currentUser } = useGetUserById(id || "");
+
+  if (!currentUser)
+    return (
+      <div className="flex-center w-full h-full">
+        <Loader />
+      </div>
+    );
+
+  return (
+    <div className="profile-container">
+      <div className="profile-inner_container">
+        <div className="flex xl:flex-row flex-col max-xl:items-center flex-1 gap-7">
+          <img
+            src={
+              currentUser.imageUrl || "/assets/icons/profile-placeholder.svg"
+            }
+            alt="profile"
+            className="w-28 h-28 lg:h-36 lg:w-36 rounded-full"
+          />
+          <div className="flex flex-col flex-1 justify-between md:mt-2">
+            <div className="flex flex-col w-full">
+              <h1 className="text-center xl:text-left h3-bold md:h1-semibold w-full">
+                {currentUser.name}
+              </h1>
+              <p className="small-regular md:body-medium text-light-3 text-center xl:text-left">
+                @{currentUser.username}
+              </p>
+            </div>
+
+            <div className="flex gap-8 mt-10 items-center justify-center xl:justify-start flex-wrap z-20">
+              <StatBlock value={currentUser.posts.length} label="Posts" />
+              <StatBlock value={20} label="Followers" />
+              <StatBlock value={20} label="Following" />
+            </div>
+
+            <p className="small-medium md:base-medium text-center xl:text-left mt-7 max-w-screen-sm">
+              {currentUser.bio}
+            </p>
+          </div>
+
+          <div className="flex justify-center gap-4">
+            <div className={`${user.id !== currentUser.$id && "hidden"}`}>
+              <Link
+                to={`/update-profile/${currentUser.$id}`}
+                className={`h-12 bg-dark-4 px-5 text-light-1 flex-center gap-2 rounded-lg ${
+                  user.id !== currentUser.$id && "hidden"
+                }`}>
+                <img
+                  src={"/assets/icons/edit.svg"}
+                  alt="edit"
+                  width={20}
+                  height={20}
+                />
+                <p className="flex whitespace-nowrap small-medium">
+                  Edit Profile
+                </p>
+              </Link>
+            </div>
+            <div className={`${user.id === id && "hidden"}`}>
+              <Button type="button" className="shad-button_primary px-8">
+                Follow
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {currentUser.$id === user.id && (
+        <div className="flex max-w-5xl w-full">
+          <Link
+            to={`/profile/${id}`}
+            className={`profile-tab rounded-l-lg ${
+              pathname === `/profile/${id}` && "!bg-dark-3"
+            }`}>
+            <img
+              src={"/assets/icons/posts.svg"}
+              alt="posts"
+              width={20}
+              height={20}
+            />
+            Posts
+          </Link>
+          <Link
+            to={`/profile/${id}/liked-posts`}
+            className={`profile-tab rounded-r-lg ${
+              pathname === `/profile/${id}/liked-posts` && "!bg-dark-3"
+            }`}>
+            <img
+              src={"/assets/icons/like.svg"}
+              alt="like"
+              width={20}
+              height={20}
+            />
+            Liked Posts
+          </Link>
+        </div>
+      )}
+
+      <Routes>
+        <Route
+          index
+          element={<GridPostList posts={currentUser.posts} showUser={false} />}
+        />
+        {currentUser.$id === user.id && (
+          <Route path="/liked-posts" element={<LikedPosts />} />
+        )}
+      </Routes>
+      <Outlet />
+    </div>
+  );
+};
+
+export default Profile;
+
+          More Related Posts
+        </h3>
+        {isUserPostLoading || !relatedPosts ? (
+          <Loader />
+        ) : (
+          <GridPostList posts={relatedPosts} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default PostDetails;
